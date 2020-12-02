16cf7a0ccca4ffbfae4117542fd1261609ecadbf
nazibalalam <nazibalalam@gmail.com>
2020-12-02 10:49:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+value = 0
+for value in range(100) :
+  print("This is the power of for loop")
+
+  // every time i put a number in the range() keyword, for loop will perform an action that many times
+  // if i put 20 in range this loop will perform the action 20 times
+  // you have to put an " : " after range(123)
+  // try this editor instead of the terminal
+  // type whatever it is you want to type HERE !!!!
