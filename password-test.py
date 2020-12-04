ac4a7571684aa3b4cbac287bdea95c727e6a7e39
nazibalalam <nazibalalam@gmail.com>
2020-12-04 11:15:58 +0000

    Just daily backup.

--- password-test.py
+++ password-test.py
@@ -3,17 +3,13 @@ secret_message = "My name is Nazib Al Alam"
 password = "I forgot the password"
 
 while True :
-  input_message = "Enter the password : "
-  guess = input()  
+  guess = input("Please enter the password")
 
   if not guess == secret_message :
-
-    input_message = "Wrong password! Try again : "
-    
-    
+    print("Wrong password! Please try again.")
+    continue
 
   if guess == secret_message :
-
     print("Congratulations! you figured out the password!")
     print('Here is the "secret_message" :')
     print(secret_message)
---
+++ text.py
@@ -0,0 +1,10 @@
+while True :
+  kant = input("type somthing (q to quit) : ")
+
+  if not kant == "q" :
+    print("You typed : " , kant)
+    continue
+  
+  if kant == "q" :
+    print("you decided to quit")
+    break
